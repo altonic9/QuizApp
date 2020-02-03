f2e52abedee536469b27b53255e7b9b79b18114b
BISASAM <39947530+BISASAM@users.noreply.github.com>
2020-02-03 13:52:16 +0100

    added some printing functions to utiliy
    print header
    &
    print nav menu and waits for user input

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,18 +1,10 @@
 public class MainMenu {
 
     public static void start() {
-        String caption = "\n\t==========================" +
-                "\n\t||\tWelcome to QUIZAPP  ||\n" +
-                "\t==========================";
-        String nav = "\n\t What would you like to do?\n" +
-                "\n\t 1. Create New Profile" +
-                "\n\t 2. Continue from existing Profile" +
-                "\n\t 3. Start Editor\n";
+        Utility.printHeader("Main Menu");
 
-        System.out.println(caption);
-        System.out.println(nav);
-
-        int menuPoint = Utility.getIntInput(1, 3);
+        String[] nav = {"Create New Profile", "Continue from existing Profile", "Start Editor"};
+        int menuPoint = Utility.printNavigation("What would you like to do?", nav);
 
         switch (menuPoint) {
             case 1:
--- src/Utility.java
+++ src/Utility.java
@@ -40,4 +40,47 @@ public class Utility {
             }
         }
     }
+
+    public static boolean getConfirmation(String text) {
+        System.out.println("\n" + text);
+        System.out.println("\n\t 1. Yes \t 2. No\n");
+        int y = getIntInput(1, 2);
+
+        return (y == 1);
+    }
+
+    public static void printHeader(String text) {
+
+        String line = "\n\t==============================\n";
+
+        int space_count = (30 - text.length()) / 2 - 1;
+        String space = "";
+        for (int i=0; i<space_count; i++) { space += " "; }
+
+        // add a space to left side if text length is not even
+        String header = line + "\t|" + space + text + space;
+        if (text.length()%2 == 0) {
+            header += "|" + line;
+        }
+        else {
+            header += " |" + line;
+        }
+
+        System.out.println(header);
+    }
+
+    public static int printNavigation(String caption, String[] points){
+        //prints a bulletpoint list and waits for user input
+
+        System.out.println("\t" + caption + "\n");
+
+        int i = 1;
+        for (String p : points) {
+            System.out.println("\t" + i + ". " + p);
+            i++;
+        }
+        System.out.println();
+        return getIntInput(1, points.length);
+    }
+
 }
