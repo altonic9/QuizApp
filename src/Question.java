ff6237590e7326f1760dce594b9c6d19333d828c
BISASAM <39947530+BISASAM@users.noreply.github.com>
2020-02-04 14:27:50 +0100

    added "text" questions

--- src/Question.java
+++ src/Question.java
@@ -2,34 +2,52 @@ public class Question {
 
     private String text;
     private String type;
-    private String[] answers;
-    private int crrAnswer;
+    private String[] answers; // possible answers for Multiple Choice
+    private int mcCrrAnswer; // correct answer for Multiple Choice
+    private String textCrrAnswer; // correct answer for Text-Questions
 
     //default constructor
     public Question()
     {
-        this.text = "undefined";
-        this.type = "undefined";
+        this.text = null;
+        this.type = null; // either "mc" or "txt"
         this.answers = null;
-        this.crrAnswer = -1;
+        this.mcCrrAnswer = -1;
+        this.textCrrAnswer = null;
     }
 
-    //constructor
+    //constructor for MC question
     public Question(String text, String type, String[] answers, int crrAnswer)
     {
         this.type = type;
         this.text = text;
         this.answers = answers;
-        this.crrAnswer = crrAnswer;
+        this.mcCrrAnswer = crrAnswer;
+    }
+
+    //constructor for text question
+    public Question(String text, String type, String crrAnswer)
+    {
+        this.type = type;
+        this.text = text;
+        this.textCrrAnswer = crrAnswer;
     }
 
     public void setText(String text) { this.text = text; }
     public void setType(String type) { this.type = type; }
     public void setAnswers(String[] answers) { this.answers = answers; }
-    public void setCrrAnswer(int crrAnswer) { this.crrAnswer = crrAnswer; }
+    // overloaded:
+    public void setCrrAnswer(int crrAnswer) { this.mcCrrAnswer = crrAnswer; }
+    public void setCrrAnswer(String crrAnswer) { this.textCrrAnswer = crrAnswer; }
 
     public String getText() {return this.text;}
     public String getType() {return this.type;}
     public String[] getAnswers() {return this.answers;}
-    public int getCrrAnswer() {return this.crrAnswer;}
+    // overloaded:
+    public Boolean isCrrAnswer(int answer) {
+        return answer == mcCrrAnswer;
+    }
+    public Boolean isCrrAnswer(String answer) {
+        return answer.toLowerCase().equals(textCrrAnswer.toLowerCase());
+    }
 }
