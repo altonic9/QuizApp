3e46bcfcfe234a67ebc76447271a91a7d64c4f67
Tom-ks <60391567+Tom-ks@users.noreply.github.com>
2020-02-04 15:43:58 +0100

    levensthein

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -3,7 +3,7 @@ public class MainMenu {
     public static void start() {
         Utility.printHeader("Main Menu");
 
-        String[] nav = {"Start Game!", "Start Editor", "Play Game (provisorisch)"};
+        String[] nav = {"Start Game!", "Start Editor", "Play Game (provisorisch)", "Exit Game"};
         int menuPoint = Utility.printNavigation("What would you like to do?", nav);
 
         switch (menuPoint) {
@@ -16,6 +16,8 @@ public class MainMenu {
             case 3:
                 Game.start();
                 break;
+            case 4:
+                System.exit(0);
         }
     }
 }
--- src/Utility.java
+++ src/Utility.java
@@ -32,16 +32,6 @@ public class Utility {
         return scanner.nextLine();
     }
 
-    public static String getStringInputMinMax(int min, int max) {
-        //funktioniert nicht!!
-        while (true) {
-            String text = scanner.nextLine();
-            if (text.length() > min && text.length() < max) {
-                return text;
-            }
-        }
-    }
-
     public static boolean getConfirmation(String text) {
         System.out.println("\n" + text);
         System.out.println("\n\t 1. Yes \t 2. No\n");
@@ -86,5 +76,39 @@ public class Utility {
 
     public static void clearScreen() {
         System.out.println(clearLine);
+    }
+
+    public static int levensthein(String str1, String str2) {
+        // prepares an array for the levensthein algorithm
+        int[][] distance = new int[str1.length()+1][str2.length()+1];
+
+        for (int x=0; x <= str1.length(); x++) {
+            distance[x][0] = x;
+        }
+
+        for (int y=0; y <= str2.length(); y++) {
+            distance[0][y] = y;
+        }
+
+        return calculateLevensthein(str1, str2, distance);
+    }
+
+    private static int calculateLevensthein(String str1, String str2, int[][] distance) {
+        // levensthein algorithm
+        int d;
+        for (int i=0; i < str1.length(); i++) {
+            for (int j=0; j < str2.length(); j++) {
+                d = 1;
+                if (str1.substring(i, i+1).equals(str2.substring(j, j+1))) {
+                    d = 0;
+                }
+                distance[i+1][j+1] = Math.min(Math.min(
+                        distance[i][j+1]+1,
+                        distance[i+1][j]+1),
+                        distance[i][j] + d
+                );
+            }
+        }
+        return distance[str1.length()][str2.length()];
     }
 }
