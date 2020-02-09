07489e972a51ee0e5df43be29a7a4383486a6dd7
BISASAM <39947530+BISASAM@users.noreply.github.com>
2020-02-09 17:22:11 +0100

    editor - question creation

--- src/gui/questionCreationController.java
+++ src/gui/questionCreationController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.event.ActionEvent;
+import quiz.Question;
 
 public class questionCreationController {
 
@@ -14,9 +15,11 @@ public class questionCreationController {
     private ComboBox<String> typeComboBox;
     @FXML
     private VBox rootVbox;
+    @FXML
+    private TextField questionTxt;
 
-    //populate choiceBox, cant do it in fxml or scenebuilder
     public void initialize() {
+        //populate choiceBox, cant do it in fxml or scenebuilder
         typeComboBox.setItems(FXCollections.observableArrayList("Multiple Choice", "Text Answer"));
     }
 
@@ -33,15 +36,24 @@ public class questionCreationController {
         rootVbox.getChildren().remove(rootVbox.lookup("#textQuestion"));
         rootVbox.getChildren().remove(rootVbox.lookup("#mc"));
 
-        //all changes are children of this Hbox
-        HBox hbox = new HBox();
-        hbox.setId("textQuestion");
-        hbox.setAlignment(Pos.CENTER);
+        //all changes are children of this Vbox
+        VBox vBox = new VBox();
+        vBox.setId("textQuestion");
+        vBox.setAlignment(Pos.CENTER);
 
+        HBox hbox = new HBox();
+        hbox.setAlignment(Pos.CENTER);
         Label l = new Label("Aswer");
         TextField t = new TextField();
+        t.setId("crrAnswer");
         hbox.getChildren().addAll(l, t);
-        rootVbox.getChildren().add(hbox);
+        vBox.getChildren().addAll(hbox);
+
+        Button btn = new Button("Add");
+        btn.setOnAction((event) -> { handleAddButton(event); });
+        vBox.getChildren().add(btn);
+
+        rootVbox.getChildren().add(vBox);
     }
 
     private void extendSceneforMultipleChoice() {
@@ -62,7 +74,7 @@ public class questionCreationController {
         cb.setItems(FXCollections.observableArrayList("1", "2", "3"));
         cb.setOnAction((event) -> {
             int i = Integer.parseInt(cb.getValue());
-            extendMcAnswers(mcBox, i);
+            extendSceneforMultipleChoice2(mcBox, i);
         });
 
 
@@ -71,7 +83,7 @@ public class questionCreationController {
         rootVbox.getChildren().add(mcBox);
     }
 
-    private void extendMcAnswers(VBox mcBox, int j) {
+    private void extendSceneforMultipleChoice2(VBox mcBox, int j) {
         //remove previously made changes to scene
         mcBox.getChildren().remove(mcBox.lookup("#answers"));
 
@@ -85,13 +97,37 @@ public class questionCreationController {
 
             Label l = new Label("Answer " + i + " ");
             TextField t = new TextField();
+            t.setId("answer"+i);
             hBox.getChildren().addAll(l, t);
             answerBox.getChildren().add(hBox);
         }
+
+        Button btn = new Button("Add");
+        btn.setOnAction((event) -> { handleAddButton(event); });
+        answerBox.getChildren().add(btn);
 
         mcBox.getChildren().add(answerBox);
 
     }
 
+    public void handleAddButton(ActionEvent event) {
+        Question q = new Question();
+        q.setText(questionTxt.getText());
 
+        String type = typeComboBox.getValue();
+        q.setType(type);
+
+        if (type == "Text Answer") {
+            // get the answer textfield
+            TextField t = (TextField) rootVbox.lookup("#crrAnswer");
+            q.setCrrAnswer(t.getText());
+        }
+        else {
+
+        }
+
+        // How to pass topics name?
+        // passing information between scenes?
+
+    }
 }
