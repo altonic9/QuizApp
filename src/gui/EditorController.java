d2c0130e9139922ddc09dcd2b77ee3e3e1ac2ecb
BISASAM <39947530+BISASAM@users.noreply.github.com>
2020-02-13 15:41:53 +0100

    added confirmation dialogs for "delete" buttons

--- src/gui/EditorController.java
+++ src/gui/EditorController.java
@@ -1,16 +1,14 @@
 package gui;
 
+import java.util.ArrayList;
+import java.util.Optional;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 
 import quiz.Question;
 import quiz.Topic;
 
-import java.util.ArrayList;
-import java.util.Optional;
 
 
 public class EditorController {
@@ -19,8 +17,6 @@ public class EditorController {
     private ListView topicsListView;
     @FXML
     private ListView questionsListView;
-    @FXML
-    private Button addTopicButton;
 
     private ArrayList<Topic> topics;
     private Topic selectedTopic; // the loaded topic
@@ -28,7 +24,6 @@ public class EditorController {
 
     public void initialize() {
         loadTopicsList();
-        initButtonsWithDialogInput();
     }
 
     private void loadTopicsList() {
@@ -37,23 +32,88 @@ public class EditorController {
 
         // get all topic names and add to listView
         topics = Topic.getAllTopics();
-
         for (Topic t : topics) {
             topicsListView.getItems().add(t.getName());
         }
     }
 
-    private void initButtonsWithDialogInput() {
-        // set action for buttons which trigger textinput dialog
-        // cant do it in fxml file, because textinputdialog is opened twice then
-        addTopicButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                String name = showInputTextDialog("New Topic", "Enter topic's title");
-                if (name != null)
-                    addTopicButton(name);
-            }
-        });
+    public void addTopicButton() {
+        String name = showInputTextDialog("New Topic", "Enter topic's title");
+        if (name != null) {
+            Topic newTopic = new Topic(name);
+            newTopic.saveToFile();
+            loadTopicsList();
+        }
+    }
+
+    public void deleteTopicButton() {
+        // get index of selected object
+        int selectedItem = topicsListView.getSelectionModel().getSelectedIndex();
+
+        if (selectedItem == -1) {
+            showAlert("Info", "No Topic selected");
+            return;
+        }
+
+        Topic selectedTopic = topics.get(selectedItem);
+        if (showConfirmationDialog("Confirmation", "Do you really want to delete \"" + selectedTopic.getName() + "\"-Topic?"))
+            selectedTopic.delete();
+
+        // update topics list
+        loadTopicsList();
+
+    }
+
+    public void loadButton() {
+        // empty list
+        questionsListView.getItems().clear();
+
+        // get index of selected item
+        int selectedItem = topicsListView.getSelectionModel().getSelectedIndex();
+        selectedTopic = topics.get(selectedItem);
+
+        // popuate question ListView
+        for (Question q : selectedTopic.getAllQuestions()) {
+            questionsListView.getItems().add(q.getText());
+        }
+    }
+
+    public void addQuestionButton() {
+
+        if (selectedTopic == null) {
+            showAlert("Info", "First, Select and Load Topic");
+            return;
+        }
+
+        Helper.topicUUID = selectedTopic.getId();
+        Main.changeScene("questionCreation.fxml");
+    }
+
+    public void deleteQuestionButton() {
+        // get index of selected item, returns -1 if none is selected
+        int selectedItem = questionsListView.getSelectionModel().getSelectedIndex();
+
+        if (selectedItem == -1) {
+            showAlert("Info", "No Question selected!");
+            return;
+        }
+
+        Question q = selectedTopic.getAllQuestions().get(selectedItem);
+        if (showConfirmationDialog("Confirmation", "Do you really want to delete \n \"" + q.getText() + "\"-Question?")) {
+            selectedTopic.deleteQuestion(q);
+            selectedTopic.saveToFile();
+        }
+
+        //update listview
+        questionsListView.getItems().clear();
+        for (Question a : selectedTopic.getAllQuestions()) {
+            questionsListView.getItems().add(a.getText());
+        }
+
+    }
+
+    public void closeButton() {
+        Main.changeScene("startWindow.fxml");
     }
 
     private static String showInputTextDialog(String title, String text) {
@@ -71,85 +131,6 @@ public class EditorController {
             return null;
     }
 
-    public void loadButton() {
-        // empty list
-        questionsListView.getItems().clear();
-
-        // get index of selected item
-        int selectedItem = topicsListView.getSelectionModel().getSelectedIndex();
-        selectedTopic = topics.get(selectedItem);
-
-        for (Question q : selectedTopic.getAllQuestions()) {
-            questionsListView.getItems().add(q.getText());
-        }
-    }
-
-    // public void saveButton() {
-    /* not needed anymore
-        for (Topic t : topics) {
-            t.saveToFile();
-        }
-    }*/
-
-    public void closeButton() {
-        Main.changeScene("startWindow.fxml");
-    }
-
-    public void addTopicButton(String name) {
-        Topic newTopic = new Topic(name);
-        newTopic.saveToFile();
-        loadTopicsList();
-    }
-
-    public void addQuestionButton() {
-
-        if (selectedTopic == null) {
-            showAlert("Info", "First, Select and Load Topic");
-            return;
-        }
-
-        Helper.topicUUID = selectedTopic.getId();
-        Main.changeScene("questionCreation.fxml");
-    }
-
-    public void deleteTopicButton() {
-        // get index of selected object
-        int selectedItem = topicsListView.getSelectionModel().getSelectedIndex();
-
-        if (selectedItem == -1) {
-            showAlert("Info", "No Topic selected");
-            return;
-        }
-
-        Topic selectedTopic = topics.get(selectedItem);
-        selectedTopic.delete();
-
-        // update topics list
-        loadTopicsList();
-
-    }
-
-    public void deleteQuestionButton() {
-        // get index of selected item, returns -1 if none is selected
-        int selectedItem = questionsListView.getSelectionModel().getSelectedIndex();
-
-        if (selectedItem == -1) {
-            showAlert("Info", "No Question selected!");
-            return;
-        }
-
-        Question q = selectedTopic.getAllQuestions().get(selectedItem);
-        selectedTopic.deleteQuestion(q);
-        selectedTopic.saveToFile();
-
-        //update listview
-        questionsListView.getItems().clear();
-        for (Question a : selectedTopic.getAllQuestions()) {
-            questionsListView.getItems().add(a.getText());
-        }
-
-    }
-
     private void showAlert(String title, String text) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title);
@@ -161,4 +142,14 @@ public class EditorController {
         alert.showAndWait();
     }
 
+    private Boolean showConfirmationDialog(String title, String text) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(text);
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        return result.get() == ButtonType.OK;
+    }
 }
