1418f6882b67f447b85e95834688ad0290e43840
BISASAM <39947530+BISASAM@users.noreply.github.com>
2020-03-10 17:51:04 +0100

    Update StartScreenController.java

--- src/gui/StartScreenController.java
+++ src/gui/StartScreenController.java
@@ -1,12 +1,17 @@
 package gui;
 
 import javafx.fxml.FXML;
+import quiz.Topic;
 
 
 public class StartScreenController {
 
 
     public void startQuiz() {
+        if (Topic.getAllTopics().size() == 0) {
+            GuiUtil.showAlert("Information", "No topics available.\nPlease go to the Editor and create a Topic first.");
+            return;
+        }
         Main.changeScene("profileSelection.fxml");
     }
 
